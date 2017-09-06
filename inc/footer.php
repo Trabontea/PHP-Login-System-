6a3f0bea27d67450be2ff4db24db63e3b8d5b09f
Trabontea <daniembotea@gmail.com>
2017-09-06 18:33:33 +0300

    Add config, login register

--- inc/footer.php
+++ inc/footer.php
@@ -1,8 +1,16 @@
+<?php 
+
+	//If there is no constant defined called __CONFIG__, do not load this file 
+	if(!defined('__CONFIG__')) {
+		exit('You do not have a config file');
+	}
+?>
+
 <!-- jQuery is required -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 <!-- UIkit JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit-icons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/js/uikit.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/js/uikit-icons.min.js"></script>
 
 <script src="/assets/js/main.js"></script>
